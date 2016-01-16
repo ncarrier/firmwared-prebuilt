6f049397dc0f96a42dbae35bbecd38513c0e7599
Nicolas Carrier <nicolas.carrier@parrot.com>
2016-01-16 15:32:28 +0100

    use pkg-config for retrieving the needed flags

--- atom.mk
+++ atom.mk
@@ -2,6 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libblkid
-LOCAL_EXPORT_LDLIBS := -lblkid
+LOCAL_EXPORT_CFLAGS := $(shell pkg-config --cflags blkid)
+LOCAL_EXPORT_LDLIBS := $(shell pkg-config --libs blkid)
 include $(BUILD_PREBUILT)
 
